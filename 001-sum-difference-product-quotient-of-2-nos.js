a99fed28f838c34665791a9d6e17df6d0ce6038d
Akash Nigam <contact.akashnigam@gmail.com>
2025-09-18 17:12:10 +0530

    Procedural JavaScript solution for printing the sum, difference, product and quotient of two numbers entered by the user

---
+++ 001-sum-difference-product-quotient-of-2-nos.js
@@ -0,0 +1,25 @@
+const prompt = require('prompt-sync')();
+
+function printResults(numX, numY) {
+    const sum = numX + numY;
+    const diff = numX - numY;
+    const product = numX * numY;
+    const quotient = numX / numY;
+
+    console.log(`Sum = ${sum}`);
+    console.log(`Difference = ${diff}`);
+    console.log(`Product = ${product}`);
+    console.log(`Quotient = ${quotient}`);
+}
+
+function main() {
+    console.log("Please enter the first number: ");
+    const firstNum = Number(prompt());
+
+    console.log("Please enter the second number: ");
+    const secondNum = Number(prompt());
+
+    printResults(firstNum, secondNum);
+}
+
+main();
